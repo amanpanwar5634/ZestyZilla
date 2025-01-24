340daac09a3e84d07074fac142c0708f422412ab
Aman Panwar <160482511+amanpanwar5634@users.noreply.github.com>
2025-01-25 01:54:37 +0530

    Update Myorder.jsx

--- Frontend/src/pages/myorders/Myorder.jsx
+++ Frontend/src/pages/myorders/Myorder.jsx
@@ -1,5 +1,5 @@
 import React from "react";
- import MyorderContent from "./myorderContent";
+ import MyorderContent from "./MyorderContent";
  import Navbar from "../../components/Navbar";
  import Footer from "../../components/Footer";
 export default function Myorder(){

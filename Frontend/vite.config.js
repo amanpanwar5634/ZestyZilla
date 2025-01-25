5c587f30eb71a479b99a8f21f159b8fc976e1556
Aman panwar <amanpanwar123op@gmail.com>
2025-01-25 14:19:24 +0530

    changes in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Example: Exclude certain dependencies from the bundle
-      external: ['react', 'react-dom'], // Prevent these from being bundled
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-});
+})
